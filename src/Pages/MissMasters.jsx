77b82baaf83a3a5b63a3c3c141f5dd1f338931e6
polymaster16 <uili.kam8@gmail.com>
2023-11-28 10:23:03 +0100

    modified:   src/Pages/MissMasters.jsx

--- src/Pages/MissMasters.jsx
+++ src/Pages/MissMasters.jsx
@@ -36,26 +36,10 @@ export default function Dropbot(){
   }
 
 
-    async function pay(amount){
+    async function pay(){
        // console.log(phoneNumber)
-       console.log(no_votes,'nov --- voa',votesToAdd)
         setLoading(true)
         try {
-          const payment = new PaymentOperation(
-            {applicationKey: 'a3586274938ddb73c5d1259fc935a2406d3d4acc ',
-             accessKey: '6e47208b-9ad0-4b53-b834-58db379cdf5c',
-              secretKey: '3e52d25f-3640-411d-bb64-3d26ce73e416'});
-        
-              const response = await payment.makeCollect(
-                paymentAmount, 
-                paymentOperator,
-                 paymentNumber, 
-                 new Date(),
-                  Signature.nonceGenerator());        
-        console.log(response.isOperationSuccess());
-        console.log(response.isTransactionSuccess());
-      
-        if(response.isOperationSuccess || response.isTransactionSuccess){
           client2.patch(candidateID)
           .set({no_votes: parseInt(no_votes)+parseInt(votesToAdd)})
           .commit()
@@ -74,16 +58,32 @@ export default function Dropbot(){
             }
 
             client2.create(newVote)
-            .then(()=>{
-              alert("Succesful transaction!! Your votes were computed.")
-              window.location.href = 'https://www.comictu.com/stats'
-            })
         
           })
           .catch((err)=>{
-           alert(err)
+           alert("An error occur, try again later. ", err)
+           window.location.href = 'https://www.comictu.com/'
           })
 
+
+
+          const payment = new PaymentOperation(
+            {applicationKey: 'a3586274938ddb73c5d1259fc935a2406d3d4acc ',
+             accessKey: '6e47208b-9ad0-4b53-b834-58db379cdf5c',
+              secretKey: '3e52d25f-3640-411d-bb64-3d26ce73e416'});
+        
+              const response = await payment.makeCollect(
+                paymentAmount, 
+                paymentOperator,
+                 paymentNumber, 
+                 new Date(),
+                  Signature.nonceGenerator());        
+        console.log(response.isOperationSuccess());
+        console.log(response.isTransactionSuccess());
+      
+        if(response.isOperationSuccess || response.isTransactionSuccess){
+          alert("Succesful transaction!! Your votes were computed.")
+          window.location.href = 'https://www.comictu.com/stats'
         }
       
         } catch (error) {
@@ -101,14 +101,8 @@ export default function Dropbot(){
         <div className='pt-[200px] pb-[500px]  px-9 bg-green-900 text-gray-50 '>
             <p className="font-bold text-xl text-center mb-2"> 🤖 COMICT-U</p>
             <p className="text-left">📰 Hello {paymentNumber}, You'll see a popup on your screen after proceeding to payment.</p>
-           <br></br>
-            <p className="text-left">🚨 DO NOT CLOSE THIS PAGE</p>
-            <p className="text-left">🚨 DO NOT CLOSE THIS PAGE</p>
-            <p className="text-left">🚨 DO NOT CLOSE THIS PAGE PLEASE</p>
-            <p className="text-left">🚨 DO NOT CLOSE THIS PAGE UNTIL YOUR TRANSACTION HAVE BEEN VALIDATED</p>
-            <br></br>
-
-            <p className="text-left">⏰ If the popup doesnt show within 45 seconds , dial {paymentOperator ==='MTN'?<span className='font-bold text-yellow-600'>*126#</span>:<span className='font-bold text-orange-600'>#150*50#</span>} ON ANOTHER DEVICE and validate the transaction. </p>
+          
+            <p className="text-left">⏰ If the popup doesnt show within 45 seconds , dial {paymentOperator ==='MTN'?<span className='font-bold text-yellow-600'>*126#</span>:<span className='font-bold text-orange-600'>#150*50#</span>}  and validate the transaction. </p>
 
             <p className="text-left">🚨🚨🚨 Make sure you keep a screenshot or screencapture of this transaction. It's very important.</p>
 
@@ -117,7 +111,7 @@ export default function Dropbot(){
             <p>Payment Amount: {paymentAmount}</p> */}
 {     !loading ?     
   <button onClick={()=>pay()} class='p-2 bg-blue-600 rounded-sm mt-4 font-bold'>CLICK ON THIS BUTTON</button>
-    :   <button class='p-2 bg-blue-600 rounded-full mt-4 font-bold spins p-2'>🪙</button>
+    :   <button class='p-2 bg-blue-600 rounded-full mt-4 font-bold spins p-2'>🗳️</button>
 
 }      
       
